861ad5f26278bdc8a87c1f188c84a77aba61c605
fornari03 <221017023@aluno.unb.br>
2025-05-07 17:37:40 -0300

    feat: implementa a camada TLS no servidor http

--- server/server.py
+++ server/server.py
@@ -1,11 +1,19 @@
 from http.server import HTTPServer
 from handler import AuthHandler
+import ssl
 
 def run():
     server_address = ('localhost', 4443) # usa a porta 4443 pra não dar conflito com o https padrão
     server = HTTPServer(server_address, AuthHandler)
 
-    # TODO: encapsular o socket com TLS para virar https
+    # cria o contexto SSL/TLS
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+
+    # adiciona o certificado e a chave privada pro SSL/TLS
+    context.load_cert_chain(certfile='certificates/cert.pem', keyfile='keys/private.pem')
+
+    # faz o encapsulamento do socket com SSL/TLS
+    server.socket = context.wrap_socket(server.socket, server_side=True)
 
     print("Server is running on https://localhost:4443")
     server.serve_forever()
